6e997fda510e19b9a51b1e42368afcc84a44ea84
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2024-05-14 22:20:48 +0100

    Conversion between basic block formats

--- DeBruijnSSA/BinPair/Syntax.lean
+++ DeBruijnSSA/BinPair/Syntax.lean
@@ -115,16 +115,41 @@ theorem Block.fv_lwk (ρ : ℕ → ℕ) (β : Block φ) : (β.lwk ρ).fv = β.fv
 theorem Block.fl_lwk (ρ : ℕ → ℕ) (β : Block φ) : (β.lwk ρ).fl = β.fl.map ρ := by
   simp [Terminator.fl_lwk]
 
+/-- Prepend a let-binding to this basic block -/
+@[match_pattern]
+def Block.let1 (a : Term φ) (β : Block φ) : Block φ where
+  body := Body.let1 a β.body
+  terminator := β.terminator
+
+/-- Prepend a destructuring let-binding to this basic block -/
+@[match_pattern]
+def Block.let2 (a : Term φ) (β : Block φ) : Block φ where
+  body := Body.let2 a β.body
+  terminator := β.terminator
+
 /-- Prepend a body of instructions to this basic block -/
 def Block.prepend (b : Body φ) (β : Block φ) : Block φ where
   body := b.append β.body
   terminator := β.terminator
 
+@[simp]
 theorem Block.prepend_nil (β : Block φ) : β.prepend Body.nil = β := rfl
+
+@[simp]
+theorem Block.prepend_let1 (a : Term φ) (b : Body φ) (β : Block φ)
+  : β.prepend (Body.let1 a b) = (β.prepend b).let1 a := rfl
+
+@[simp]
+theorem Block.prepend_let2 (a : Term φ) (b : Body φ) (β : Block φ)
+  : β.prepend (Body.let2 a b) = (β.prepend b).let2 a := rfl
 
 theorem Block.prepend_append (b b' : Body φ) (β : Block φ)
   : β.prepend (b.append b') = (β.prepend b').prepend b := by
   simp only [Block.prepend, Body.append_assoc]
+
+@[simp]
+theorem Block.prepend_body_terminator (β : Block φ)
+  : (β.terminator : Block φ).prepend β.body = β := by cases β; simp [prepend, Terminator.toBlock]
 
 -- TODO: prepend_nil, pretty trivial
 
@@ -145,6 +170,112 @@ theorem Block.ltimes_nil : (β : Block φ) → β.ltimes Body.nil = β
   | ⟨body, terminator⟩ => by
     simp only [ltimes, prepend, vwk, Body.num_defs, add_zero, Body.nil_append, mk.injEq]
     exact ⟨Body.wk_id _, Nat.liftnWk_id body.num_defs ▸ Terminator.vwk_id _⟩
+
+-- TODO: version with better defeqs?
+
+/-- Prepend a let-binding to this region -/
+@[simp]
+def BBRegion.let1 (a : Term φ) : BBRegion φ → BBRegion φ
+  | cfg β n G => cfg (β.let1 a) n G
+
+/-- Prepend a destructuring let-binding to this region -/
+@[simp]
+def BBRegion.let2 (a : Term φ) : BBRegion φ → BBRegion φ
+  | cfg β n G => cfg (β.let2 a) n G
+
+/-- Prepend a body of instructions to this region -/
+def BBRegion.prepend (b : Body φ) : BBRegion φ → BBRegion φ
+  | cfg β n G => cfg (β.prepend b) n G
+
+theorem BBRegion.prepend_nil (r : BBRegion φ) : r.prepend Body.nil = r := by
+  cases r; rfl
+
+theorem BBRegion.prepend_let1 (a : Term φ) (b : Body φ) (r : BBRegion φ)
+  : r.prepend (Body.let1 a b) = (r.prepend b).let1 a := by cases r; rfl
+
+theorem BBRegion.prepend_let2 (a : Term φ) (b : Body φ) (r : BBRegion φ)
+  : r.prepend (Body.let2 a b) = (r.prepend b).let2 a := by cases r; rfl
+
+/-- Precompose a body of instructions to this region -/
+@[simp]
+def TRegion.prepend (b : Body φ) (r : TRegion φ) : TRegion φ := match b with
+  | Body.nil => r
+  | Body.let1 a b => (r.prepend b).let1 a
+  | Body.let2 a b => (r.prepend b).let2 a
+
+theorem TRegion.prepend_nil (r : TRegion φ) : r.prepend Body.nil = r := rfl
+
+theorem TRegion.prepend_let1 (a : Term φ) (b : Body φ) (r : TRegion φ)
+  : r.prepend (Body.let1 a b) = (r.prepend b).let1 a := rfl
+
+theorem TRegion.prepend_let2 (a : Term φ) (b : Body φ) (r : TRegion φ)
+  : r.prepend (Body.let2 a b) = (r.prepend b).let2 a := rfl
+
+/-- Convert this region to the terminator based format -/
+def BBRegion.toTRegion : BBRegion φ → TRegion φ
+  | cfg ⟨b, t⟩ n G => (TRegion.cfg t n (λi => (G i).toTRegion)).prepend b
+
+theorem BBRegion.let1_toTRegion (a : Term φ) (r : BBRegion φ)
+  : (r.let1 a).toTRegion = r.toTRegion.let1 a := by cases r; rfl
+
+theorem BBRegion.let2_toTRegion (a : Term φ) (r : BBRegion φ)
+  : (r.let2 a).toTRegion = r.toTRegion.let2 a := by cases r; rfl
+
+theorem BBRegion.prepend_toTRegion (b : Body φ) (r : BBRegion φ)
+  : (r.prepend b).toTRegion = r.toTRegion.prepend b := by
+    induction b
+      <;> simp only [prepend_let1, prepend_let2, prepend_nil, let1_toTRegion, let2_toTRegion, *]
+      <;> rfl
+
+/-- Convert this region to the standard basic-block based format -/
+def TRegion.toBBRegion : TRegion φ → BBRegion φ
+  | TRegion.let1 a t => BBRegion.let1 a t.toBBRegion
+  | TRegion.let2 a t => BBRegion.let2 a t.toBBRegion
+  | TRegion.cfg β n G => BBRegion.cfg β n (λi => (G i).toBBRegion)
+
+theorem TRegion.let1_toBBRegion (a : Term φ) (r : TRegion φ)
+  : (r.let1 a).toBBRegion = r.toBBRegion.let1 a := rfl
+
+theorem TRegion.let2_toBBRegion (a : Term φ) (r : TRegion φ)
+  : (r.let2 a).toBBRegion = r.toBBRegion.let2 a := rfl
+
+theorem TRegion.prepend_toBBRegion (b : Body φ) (r : TRegion φ)
+  : (r.prepend b).toBBRegion = r.toBBRegion.prepend b := by
+  induction b <;> simp only [
+    prepend_let1, let1_toBBRegion, prepend_let2, let2_toBBRegion, prepend_nil,
+    BBRegion.prepend_let1, BBRegion.prepend_let2, BBRegion.prepend_nil, *]
+
+theorem TRegion.toTRegion_toBBRegion (r : TRegion φ) : r.toBBRegion.toTRegion = r := by
+  induction r with
+  | let1 _ _ I => rw [toBBRegion, BBRegion.let1_toTRegion, I]
+  | let2 _ _ I => rw [toBBRegion, BBRegion.let2_toTRegion, I]
+  | cfg => simp [toBBRegion, BBRegion.toTRegion, *]
+
+theorem BBRegion.toBBRegion_toTRegion (r : BBRegion φ) : r.toTRegion.toBBRegion = r := by
+  induction r with
+  | cfg β n G I => simp [toTRegion, TRegion.prepend_toBBRegion, TRegion.toBBRegion, prepend, I]
+
+/-- Construct a region in terminator format from one in basic-block format -/
+def TRegion.ofBBRegion : BBRegion φ ≃ TRegion φ where
+  toFun := BBRegion.toTRegion
+  invFun := TRegion.toBBRegion
+  left_inv := BBRegion.toBBRegion_toTRegion
+  right_inv := TRegion.toTRegion_toBBRegion
+
+/-- Construct a region in basic-block format from one in terminator format -/
+def BBRegion.ofTRegion : TRegion φ ≃ BBRegion φ where
+  toFun := TRegion.toBBRegion
+  invFun := BBRegion.toTRegion
+  left_inv := TRegion.toTRegion_toBBRegion
+  right_inv := BBRegion.toBBRegion_toTRegion
+
+theorem TRegion.symm_ofBBRegion : (@TRegion.ofBBRegion φ).symm = BBRegion.ofTRegion := rfl
+
+theorem BBRegion.symm_ofTRegion : (@BBRegion.ofTRegion φ).symm = TRegion.ofBBRegion := rfl
+
+-- TODO: intercoercions?
+
+-- TODO: toBBRegion_toTRegion ==> equivalence, yay
 
 -- theorem Block.ltimes_ltimes (b b' : Body φ) (β : Block φ)
 --   : β.ltimes (b.ltimes b') = (β.ltimes b').ltimes b := by
@@ -201,6 +332,7 @@ theorem Terminator.toRegion_vsubst (σ : Term.Subst φ) (t : Terminator φ)
 theorem Terminator.coe_toRegion_vsubst (σ : Term.Subst φ) (t : Terminator φ)
   : (t.vsubst σ : Region φ) = (t : Region φ).vsubst σ := toRegion_vsubst σ t
 
+/-- Convert a terminator substitution to a region substitution pointwise -/
 def Terminator.Subst.toRegion (σ : Terminator.Subst φ) : Region.Subst φ := λn => σ n
 
 theorem Terminator.Subst.toRegion_lift (σ : Terminator.Subst φ) : σ.lift.toRegion = σ.toRegion.lift := by
@@ -269,6 +401,19 @@ theorem TRegion.coe_toRegion_vsubst (σ : Term.Subst φ) (t : TRegion φ)
 
 -- TODO: BBCFG.toCFG
 
+/-- Conver a basic block CFG to a terminator CFG -/
+def BBCFG.toTCFG : BBCFG φ ≃ TCFG φ where
+  toFun := λG => ⟨G.length, TRegion.ofBBRegion ∘ G.targets⟩
+  invFun := λG => ⟨G.length, TRegion.ofBBRegion.symm ∘ G.targets⟩
+  left_inv := λ⟨_, _⟩ => by simp [<-Function.comp.assoc]
+  right_inv := λ⟨_, _⟩ => by simp [<-Function.comp.assoc]
+
+/-- Convert a terminator CFG to a basic block CFG -/
+def TCFG.toBBCFG : TCFG φ ≃ BBCFG φ := BBCFG.toTCFG.symm
+
+-- TODO: intercoercions?
+
+/-- Convert a terminator CFG to a plain CFG -/
 def TCFG.toCFG (G : TCFG φ) : CFG φ where
   length := G.length
   targets := λ i => (G.targets i).toRegion
@@ -287,19 +432,19 @@ instance : Coe (TCFG φ) (CFG φ) := ⟨TCFG.toCFG⟩
 
 -- TODO: normalize TRegion to Region; commutes with label-substiution
 
-/-- A single-entry multiple-exit region, applying a substitution on jumps -/
-inductive SRegion (φ : Type) : Type
-  | br : Nat → Term.Subst φ → SRegion φ
-  | ite : Term φ → SRegion φ → SRegion φ → SRegion φ
-  | let1 : Term φ → SRegion φ → SRegion φ
-  | let2 : Term φ → SRegion φ → SRegion φ
-  | cfg (β : SRegion φ) (n : Nat) : (Fin n → SRegion φ) → SRegion φ
+-- /-- A single-entry multiple-exit region, applying a substitution on jumps -/
+-- inductive SRegion (φ : Type) : Type
+--   | br : Nat → Term.Subst φ → SRegion φ
+--   | ite : Term φ → SRegion φ → SRegion φ → SRegion φ
+--   | let1 : Term φ → SRegion φ → SRegion φ
+--   | let2 : Term φ → SRegion φ → SRegion φ
+--   | cfg (β : SRegion φ) (n : Nat) : (Fin n → SRegion φ) → SRegion φ
 
-/-- A control-flow graph with `length` entry-point regions -/
-structure SCFG (φ : Type) : Type where
-  /-- The number of entry points to this CFG -/
-  length : Nat
-  /-- The number of exits for this CFG -/
-  targets : Fin length → SRegion φ
+-- /-- A control-flow graph with `length` entry-point regions -/
+-- structure SCFG (φ : Type) : Type where
+--   /-- The number of entry points to this CFG -/
+--   length : Nat
+--   /-- The number of exits for this CFG -/
+--   targets : Fin length → SRegion φ
 
 end BinPair
