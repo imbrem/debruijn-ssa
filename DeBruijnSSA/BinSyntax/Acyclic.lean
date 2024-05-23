c1c98672f4cc3181502a155e874f86103d04eaf7
Jad Ghalayini <jeg74@cl.cam.ac.uk>
2024-05-23 19:21:46 +0100

    Further acyclicity lore

--- DeBruijnSSA/BinSyntax/Acyclic.lean
+++ DeBruijnSSA/BinSyntax/Acyclic.lean
@@ -15,10 +15,11 @@ inductive SAcyclic' : Set ℕ → Set (Region φ)
   | cfg : SAcyclic' ((· + n) '' D) β →
     (∀i : Fin n, SAcyclic' ((· + n) '' D ∪ Set.Iio n) (G i)) → SAcyclic' D (cfg β n G)
 
-abbrev SAcyclic : Region φ → Prop := SAcyclic' ∅
-
 def Predecessors (G : Fin n → Region φ) (i : ℕ) : Set ℕ
   := { j | ∃h : j < n, i ∈ (G ⟨j, h⟩).fl }
+
+theorem Predecessors.mem_fl {G : Fin n → Region φ} {i j} (h : j ∈ Predecessors G i)
+  : i ∈ (G ⟨j, h.1⟩).fl := h.2
 
 theorem Predecessors.comp_vwk (ρ) : Predecessors (vwk ρ ∘ G) = Predecessors G := by
   funext i
@@ -27,13 +28,25 @@ theorem Predecessors.comp_vwk (ρ) : Predecessors (vwk ρ ∘ G) = Predecessors 
 theorem Predecessors.vwk (ρ)
   : @Predecessors φ n (λi => (G i).vwk ρ) = Predecessors G := comp_vwk ρ
 
+def Successors (G : Fin n → Region φ) (i : ℕ) : Set ℕ
+  := if h : i < n then { j | j ∈ (G ⟨i, h⟩).fl } else ∅
+
+theorem Successors.comp_vwk (ρ) : Successors (vwk ρ ∘ G) = Successors G := by
+  funext i
+  simp only [Successors, Function.comp_apply, fl_vwk]
+
+theorem Successors.vwk (ρ) : @Successors n φ (λi => (G i).vwk ρ) = Successors G := comp_vwk ρ
+
 def Ancestors (G : Fin n → Region φ) : ℕ → Set ℕ
   := Relation.TransGen (Predecessors G)
 
-theorem Ancestors.lt_n {G : Fin n → Region φ} (h : Ancestors G i j) : j < n := by
+theorem Ancestors.lt {G : Fin n → Region φ} (h : Ancestors G i j) : j < n := by
   cases h with
   | single hr => exact hr.1
   | tail _ hr => exact hr.1
+
+theorem Ancestors.of_mem_fl {G : Fin n → Region φ} (h : i ∈ (G j).fl)
+  : ↑j ∈ Ancestors G i := Relation.TransGen.single ⟨j.prop, h⟩
 
 theorem Ancestors.comp_vwk (ρ) : Ancestors (vwk ρ ∘ G) = Ancestors G := by
   simp only [Ancestors, Predecessors.comp_vwk]
@@ -45,8 +58,12 @@ theorem Ancestors.vwk (ρ) : @Ancestors n φ (λi => (G i).vwk ρ) = Ancestors G
 def SelfAncestors (G : Fin n → Region φ) : ℕ → Set ℕ
   := Relation.ReflTransGen (Predecessors G)
 
-theorem SelfAncestors.of_eq {G : Fin n → Region φ} {i j} (p : i = j)
+theorem SelfAncestors.eq_mem {G : Fin n → Region φ} {i j} (p : i = j)
   : i ∈ SelfAncestors G j := p ▸ Relation.ReflTransGen.refl
+
+@[simp]
+theorem SelfAncestors.self_mem {G : Fin n → Region φ} (i) : i ∈ SelfAncestors G i
+  := Relation.ReflTransGen.refl
 
 theorem SelfAncestors.ancestor_or_eq {G : Fin n → Region φ}
   (h : SelfAncestors G i j) : j ∈ Ancestors G i ∨ j = i
@@ -67,7 +84,7 @@ theorem SelfAncestors.of_ancestor {G : Fin n → Region φ} (h : Ancestors G i j
 
 theorem Ancestors.union_singleton_self {G : Fin n → Region φ} (i) :
   Ancestors G i ∪ {i} = SelfAncestors G i := Set.ext λ_ => ⟨
-    λh => h.elim SelfAncestors.of_ancestor SelfAncestors.of_eq,
+    λh => h.elim SelfAncestors.of_ancestor SelfAncestors.eq_mem,
     λh => h.ancestor_or_eq⟩
 
 theorem SelfAncestors.lt_or_eq {G : Fin n → Region φ} (h : SelfAncestors G i j) : j < n ∨ j = i
@@ -80,6 +97,25 @@ theorem SelfAncestors.comp_vwk (ρ) : SelfAncestors (vwk ρ ∘ G) = SelfAncesto
 
 theorem SelfAncestors.vwk (ρ) : @SelfAncestors n φ (λi => (G i).vwk ρ) = SelfAncestors G
   := comp_vwk ρ
+
+theorem Ancestors.split {G : Fin n → Region φ} (h : Ancestors G i j)
+  : ∃k, i ∈ (G k).fl ∧ j ∈ SelfAncestors G k := by
+  induction h with
+  | single hr => exact ⟨⟨_, hr.1⟩, hr.2, SelfAncestors.self_mem _⟩
+  | tail _ hr I =>
+    have ⟨k, hki, hkj⟩ := I
+    exact ⟨k, hki, hkj.tail hr⟩
+
+theorem Ancestors.of_split {G : Fin n → Region φ} {k}
+  (hki : i ∈ (G k).fl) (hkj : j ∈ SelfAncestors G k)
+  : Ancestors G i j := by
+  induction hkj with
+  | refl => exact Relation.TransGen.single ⟨k.prop, hki⟩
+  | tail _ hr I => exact I.tail hr
+
+theorem Ancestors.split_iff (G : Fin n → Region φ) (i j)
+  : Ancestors G i j ↔ ∃k, i ∈ (G k).fl ∧ j ∈ SelfAncestors G k
+  := ⟨Ancestors.split, λ⟨_, hki, hkj⟩ => Ancestors.of_split hki hkj⟩
 
 inductive Acyclic' : Set ℕ → Set (Region φ)
   | br {D ℓ} (h : ℓ ∉ D) e : Acyclic' D (br ℓ e)
@@ -143,7 +179,6 @@ theorem SAcyclic'.acyclic' (h : SAcyclic' D r) : Acyclic' D r := by
       | inl h => exact lt_of_lt_of_le h (le_refl _)
       | inr h => exact h ▸ i.prop
 
-
 theorem Acyclic'.not_mem_fl (h : Acyclic' D r) : ∀i ∈ D, i ∉ r.fl := by
   intro i hi
   induction h generalizing i with
@@ -183,6 +218,17 @@ theorem Acyclic'.vwk (ρ) (h : Acyclic' D r) : Acyclic' D (vwk ρ r) := by
     apply IG
   | _ => constructor <;> apply_assumption
 
+inductive SAcyclic : Set (Region φ)
+  | br ℓ e : SAcyclic (br ℓ e)
+  | case e : SAcyclic s → SAcyclic t → SAcyclic (case e s t)
+  | let1 e : SAcyclic s → SAcyclic (let1 e s)
+  | let2 e : SAcyclic s → SAcyclic (let2 e s)
+  | cfg : SAcyclic β →
+    (∀i : Fin n, SAcyclic (G i)) →
+    (∀i : Fin n, ∀j ∈ Ancestors G i, n ≤ j) → SAcyclic (cfg β n G)
+
+-- TODO: relationship between SAcyclic and SACyclic'
+
 inductive Acyclic : Set (Region φ)
   | br ℓ e : Acyclic (br ℓ e)
   | case e : Acyclic s → Acyclic t → Acyclic (case e s t)
@@ -208,25 +254,79 @@ theorem Acyclic.let1_iff (e s) : @Acyclic φ (Region.let1 e s) ↔ Acyclic s
 theorem Acyclic.let2_iff (e s) : @Acyclic φ (Region.let2 e s) ↔ Acyclic s
   := ⟨λh => by cases h; assumption, λh => let2 _ h⟩
 
+theorem Acyclic.not_mem_ancestors' (h : @Acyclic φ (Region.cfg β n G)) :
+  ∀i : Fin n, ↑i ∉ Ancestors G i := by cases h; assumption
+
+theorem Acyclic.not_mem_ancestors (h : @Acyclic φ (Region.cfg β n G)) : ∀i, i ∉ Ancestors G i
+  := λi hi => h.not_mem_ancestors' ⟨i, hi.lt⟩ hi
+
 @[simp]
 theorem Acyclic.cfg_iff (β n G) :
   @Acyclic φ (Region.cfg β n G) ↔
   Acyclic β ∧ (∀i : Fin n, Acyclic (G i)) ∧ (∀i : Fin n, ↑i ∉ Ancestors G i)
   := ⟨λh => by cases h; repeat first | constructor | assumption, λ⟨hβ, hG, hJ⟩ => cfg hβ hG hJ⟩
 
+theorem Acyclic'.acyclic (h : Acyclic' D r) : Acyclic r := by
+  induction h with
+  | @cfg n _ _ _ _ hG Iβ IG =>
+    apply Acyclic.cfg Iβ IG
+    intro i hi
+    have ⟨k, hik, hki⟩ := hi.split
+    apply (hG k).mem_fl _ hik
+    simp [hki]
+  | _ => constructor <;> assumption
+
+theorem Acyclic'.of_acyclic {r : Region φ} (h : Acyclic r) (hD : ∀x ∈ r.fl, x ∉ D) : Acyclic' D r
+  := by
+  induction h generalizing D with
+  | br => exact Acyclic'.br (hD _ (by simp)) _
+  | case e _ _ Is It =>
+    exact Acyclic'.case e
+      (Is (λi hi => hD i (by simp [hi])))
+      (It (λi hi => hD i (by simp [hi])))
+  | let1 e _ Is => exact Acyclic'.let1 e (Is (λi hi => hD i (by simp [hi])))
+  | let2 e _ Is => exact Acyclic'.let2 e (Is (λi hi => hD i (by simp [hi])))
+  | cfg _ _ hG Iβ IG =>
+    apply Acyclic'.cfg
+    . apply Iβ
+      intro i hi
+      simp only [Set.mem_image, not_exists, not_and]
+      intro j hj hj'
+      cases hj'
+      apply hD j
+      simp [Multiset.mem_liftnFv, hi]
+      exact hj
+    . intro i
+      apply IG
+      intro j hj
+      simp only [Set.mem_union, Set.mem_image, not_or, not_exists, not_and]
+      constructor
+      . intro x hx c
+        cases c
+        apply hD x
+        simp only [fl, Multiset.mem_add, Multiset.mem_liftnFv]
+        right
+        apply (Multiset.mem_finsum _ _ _).mpr
+        simp only [Finset.mem_univ, Multiset.mem_liftnFv, true_and]
+        exact ⟨_, hj⟩
+        exact hx
+      . intro c
+        have c' := Ancestors.of_split hj c
+        apply hG ⟨j, c'.lt⟩ c'
+
+theorem Acyclic'.acyclic_iff (r : Region φ) : Acyclic' D r ↔ Acyclic r ∧ ∀x ∈ r.fl, x ∉ D :=
+  ⟨λh => ⟨h.acyclic, h.mem_fl⟩, λ⟨h, h'⟩ => of_acyclic h h'⟩
+
 theorem Acyclic.acyclic_iff (r : Region φ) : Acyclic r ↔ Acyclic' ∅ r := by
-  induction r with
-  | cfg β n G Iβ IG =>
-    simp only [cfg_iff, Acyclic'.cfg_iff, Set.image_empty, Set.empty_union, and_congr_right_iff, *]
-    intro _
-    sorry
-  | _ => simp [*]
+  simp [Acyclic'.acyclic_iff]
 
--- theorem Acyclic.eq_acyclic : @Acyclic φ = Acyclic' ∅ := Set.ext acyclic_iff
+theorem Acyclic.eq_acyclic : @Acyclic φ = Acyclic' ∅ := Set.ext acyclic_iff
 
--- theorem Acyclic.subset_acyclic' : @Acyclic φ ⊆ Acyclic' D := sorry
+-- TODO: acyclicity is preserved by weakening
 
 -- TODO: acyclicity is preserved by acyclic substitution
+
+-- TODO: strict acyclicity is preserved by weakening
 
 -- TODO: strict acyclicity is preserved by strict acyclic substitution
 
