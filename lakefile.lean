9fd3f0b970393ba71b8ae48322874aa1f5ccc3ad
Jad Ghalayini <jad.ghalayini@protonmail.com>
2024-11-07 16:11:39 +0000

    Artifact

--- lakefile.lean
+++ lakefile.lean
@@ -11,9 +11,11 @@ package «DeBruijnSSA» where
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
+    @ "9248301da2c228348b194c74be94b0e0b0bbbb2c"
 
 require discretion from git
-  "https://github.com/imbrem/discretion.git" @ "main"
+  "https://github.com/imbrem/discretion.git"
+    @ "bdab441c6e43d326b72b8d1ddc8c61b96054bdf3"
 
 meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
 require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
